6dfc69465a5a24a854a87e7ecc7548b048545088
MD : ASHRAFUL ISLAM <97118095+Ashraful-89@users.noreply.github.com>
2023-01-03 04:00:02 +0600

    Create 281A.cpp

---
+++ Contitions/281A.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char str[1001];
+    gets(str);
+    if(str[0]>='a'&&str[0]<='z')
+     {
+         str[0]=str[0]-32;
+     }
+     puts(str);
+    return 0;
+}
