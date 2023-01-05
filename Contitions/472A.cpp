08c6fc86fa17ea5a6bb7bc0a17ad531ba0667241
MD : ASHRAFUL ISLAM <97118095+Ashraful-89@users.noreply.github.com>
2023-01-05 17:29:13 +0600

    Create 472A.cpp

---
+++ Contitions/472A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+  int n ;
+  cin >> n;
+  if(n % 2 == 0) cout << 6 << " " << n - 6 << endl;
+  else {
+    cout << 9 << " " << n - 9 << endl;
+  }
+  return 0;
+}
