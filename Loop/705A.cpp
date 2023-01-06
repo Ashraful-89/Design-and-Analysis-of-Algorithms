a99f9c283a71a700a0c348b54543911722fbe742
MD : ASHRAFUL ISLAM <97118095+Ashraful-89@users.noreply.github.com>
2023-01-06 22:05:43 +0600

    Create 705A.cpp

---
+++ Loop/705A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int n ;
+  cin >> n;
+  for(int i = 1 ; i <= n ; i++) {
+    if(i % 2 != 0 ) {
+      cout << "I hate ";
+      if(i == n) cout << "it ";
+      else cout << "that ";
+    }
+    if(i % 2 == 0 ) {
+      cout << "I love ";
+      if(i == n) cout << "it ";
+      else cout << "that ";
+    }
+  }
+  return 0;
+}
