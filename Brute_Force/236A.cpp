a09389bd21fe51664245e6598f8b90d76579733d
MD : ASHRAFUL ISLAM <97118095+Ashraful-89@users.noreply.github.com>
2023-01-18 15:31:14 +0600

    Update 236A.cpp

--- Brute_Force/236A.cpp
+++ Brute_Force/236A.cpp
@@ -37,3 +37,23 @@ int main()
         cout<<"CHAT WITH HER!"<<endl;
     return 0;
 }
+
+
+
+// Another solution using STL
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    string s;
+    cin >> s;
+    set < char > st;
+    for(int i = 0; i < s.size() ; i++) {
+      st.insert(s[i]);
+    }
+   if(st.size() % 2 == 0) cout << "CHAT WITH HER!" << endl;
+   else cout << "IGNORE HIM!" << endl;
+   return 0;
+}
