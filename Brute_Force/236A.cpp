c60f4b1d2520382c2aa51b5346af30ed02c977d4
MD : ASHRAFUL ISLAM <97118095+Ashraful-89@users.noreply.github.com>
2023-01-18 15:17:36 +0600

    Create 236A.cpp

---
+++ Brute_Force/236A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    char str[101];
+    int count=0,count1=0,len=0;
+    gets(str);
+    for(int k=0;str[k]!='\0';k++)
+    {
+        len++;
+    }
+    char temp;
+    for(int i=0;i<len;i++)
+    {
+        for(int j=i+1;j<len;j++)
+        {
+            if(str[j]<str[i])
+            {
+                temp=str[i];
+                str[i]=str[j];
+                str[j]=temp;
+            }
+        }
+ 
+    }
+    for(int l=0;l<len;l++)
+    {
+        if(str[l]==str[l+1])
+            count=1;
+        else
+        count1++;
+    }
+    if(count1%2!=0)
+        cout<<"IGNORE HIM!"<<endl;
+        else
+        cout<<"CHAT WITH HER!"<<endl;
+    return 0;
+}
